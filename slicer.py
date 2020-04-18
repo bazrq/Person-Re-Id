4aa70b07358c2e0045fe5fc7de3eb4c55519ea4d
bazrq <56867414+bazrq@users.noreply.github.com>
2020-04-19 04:32:59 +1000

    slicer.py
    added functionality to pick files from videos folder for slicing.

--- slicer.py
+++ slicer.py
@@ -1,15 +1,30 @@
 import cv2
 import os
+from os import listdir
+from os.path import isfile, join
 
 #make out put directory (images)
 if not os.path.exists('images'):
     os.makedirs('images')
 
-#path to dir
+#path to out dir
 outDir="images/"
 
+# create way to look into video dir and select video
+entries = os.listdir('videos/')
+print("----Slicer Script----")
+print("Note: Before ypu begin, please place files into the VIDEOS dir for slicing.")
+count=1
+for entry in entries:
+	print(str(count)+":"+entry)
+	count=count+1
+choice = input("Select which file you want sliced: ")
+refChoice= int(choice)-1
+videoInput = entries[refChoice]
+print(videoInput)
 #slice video into jpg files
-vidcap = cv2.VideoCapture("umtitti.mp4")
+
+vidcap = cv2.VideoCapture("videos/"+str(videoInput))
 success,image = vidcap.read()
 count = 0
 while success:
